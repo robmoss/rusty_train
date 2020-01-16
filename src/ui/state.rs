1b8cca266b82c3ad218562757c5703da289d3257
Rob Moss <robm.dev@gmail.com>
2020-01-17 08:25:21 +1100

    Add UI support for selecting a token for routing
    This state highlights all matching tokens on the map.

--- src/ui/state.rs
+++ src/ui/state.rs
@@ -1,5 +1,6 @@
 use cairo::{Context, Format, ImageSurface};
 use gtk::{DialogExt, FileChooserExt, GtkWindowExt, Inhibit, WidgetExt};
+use std::collections::HashMap;
 
 use crate::hex::Hex;
 use crate::map::{HexAddress, Map, RotateCW, Token, TokensTable};
@@ -70,6 +71,14 @@ pub struct EditTokens {
     original_tokens: TokensTable,
 }
 
+/// Selecting a company's tokens for route-finding.
+pub struct SelectToken {
+    active_hex: HexAddress,
+    token_spaces: Vec<TokenSpace>,
+    selected: usize,
+    matches: HashMap<HexAddress, Vec<TokenSpace>>,
+}
+
 impl Default {
     pub fn new(map: &Map) -> Self {
         Default {
@@ -123,6 +132,57 @@ impl EditTokens {
             token_spaces: token_spaces,
             selected: 0,
             original_tokens: original_tokens,
+        })
+    }
+}
+
+fn token_matches(
+    map: &Map,
+    token_opt: Option<&Token>,
+) -> HashMap<HexAddress, Vec<TokenSpace>> {
+    let pairs = token_opt
+        .map(|token| {
+            map.find_placed_tokens(token)
+                .iter()
+                .map(|(addr, _state, _tile, token_space)| {
+                    (**addr, **token_space)
+                })
+                .collect()
+        })
+        .unwrap_or(vec![]);
+    let mut matches = HashMap::new();
+    for (addr, token_space) in pairs {
+        let spaces = matches.entry(addr).or_insert(vec![]);
+        spaces.push(token_space)
+    }
+    matches
+}
+
+impl SelectToken {
+    fn try_new(map: &Map, addr: HexAddress) -> Option<Self> {
+        let hex_state = if let Some(hex_state) = map.get_hex(addr) {
+            hex_state
+        } else {
+            return None;
+        };
+        let tile = if let Some(tile) = map.tile_at(addr) {
+            tile
+        } else {
+            return None;
+        };
+        let token_spaces = tile.token_spaces();
+        if token_spaces.is_empty() {
+            return None;
+        }
+        let selected = 0;
+        let space = token_spaces[selected];
+        let token_opt = hex_state.get_token_at(&space);
+        let matches = token_matches(map, token_opt);
+        Some(SelectToken {
+            active_hex: addr,
+            token_spaces: token_spaces,
+            selected: selected,
+            matches: matches,
         })
     }
 }
@@ -343,6 +403,19 @@ impl State for Default {
             gdk::enums::key::t | gdk::enums::key::T => {
                 if let Some(addr) = self.active_hex {
                     if let Some(state) = EditTokens::try_new(map, addr) {
+                        (Box::new(state), Inhibit(false), Action::Redraw)
+                    } else {
+                        (self, Inhibit(false), Action::None)
+                    }
+                } else {
+                    (self, Inhibit(false), Action::None)
+                }
+            }
+            gdk::enums::key::r | gdk::enums::key::R => {
+                // Allow the user to select tokens, and highlight all matching
+                // tokens on the map.
+                if let Some(addr) = self.active_hex {
+                    if let Some(state) = SelectToken::try_new(map, addr) {
                         (Box::new(state), Inhibit(false), Action::Redraw)
                     } else {
                         (self, Inhibit(false), Action::None)
@@ -776,6 +849,159 @@ impl State for EditTokens {
                 map.get_hex_mut(self.active_hex)
                     .map(|hex_state| hex_state.remove_token_at(token_space));
                 (self, Inhibit(false), Action::Redraw)
+            }
+            _ => (self, Inhibit(false), Action::None),
+        }
+    }
+
+    fn button_press(
+        self: Box<Self>,
+        _hex: &Hex,
+        _map: &mut Map,
+        _window: &gtk::ApplicationWindow,
+        _area: &gtk::DrawingArea,
+        _event: &gdk::EventButton,
+    ) -> (Box<dyn State>, Inhibit, Action) {
+        (self, Inhibit(false), Action::None)
+    }
+}
+
+impl State for SelectToken {
+    fn draw(
+        &self,
+        hex: &Hex,
+        map: &Map,
+        _width: i32,
+        _height: i32,
+        ctx: &Context,
+    ) {
+        let mut hex_iter = map.hex_iter(hex, ctx);
+
+        for _ in &mut hex_iter {
+            // Draw a thick black border on all hexes.
+            // This will give map edges a clear border.
+            ctx.set_source_rgb(0.0, 0.0, 0.0);
+            hex.define_boundary(ctx);
+            ctx.set_line_width(hex.max_d * 0.05);
+            ctx.stroke();
+        }
+
+        hex_iter.restart();
+        for (_addr, tile_opt) in &mut hex_iter {
+            if let Some((tile, token_spaces)) = tile_opt {
+                // Draw the tile and any tokens.
+                tile.draw(ctx, hex);
+                for (token_space, map_token) in token_spaces.iter() {
+                    tile.define_token_space(&token_space, &hex, ctx);
+                    map_token.draw_token(&hex, ctx);
+                }
+            } else {
+                // Draw an empty hex.
+                // TODO: draw "crosshairs" at hex intersections?
+                ctx.set_source_rgb(0.7, 0.7, 0.7);
+                hex.define_boundary(ctx);
+                ctx.set_line_width(hex.max_d * 0.01);
+                ctx.stroke();
+            }
+        }
+
+        // Highlight all matching token spaces on the map.
+        hex_iter.restart();
+        for (addr, tile_opt) in &mut hex_iter {
+            if let Some(spaces) = self.matches.get(&addr) {
+                // Highlight and/or fill token spaces
+                if let Some((tile, _tokens)) = tile_opt {
+                    for token_space in spaces {
+                        let (r, g, b, a) = (0.9, 0.1, 0.1, 0.25);
+                        tile.define_token_space(token_space, hex, ctx);
+                        ctx.set_source_rgb(r, g, b);
+                        ctx.set_line_width(hex.max_d * 0.025);
+                        ctx.stroke_preserve();
+                        if self.active_hex != addr {
+                            ctx.set_source_rgba(r, g, b, a);
+                            ctx.fill_preserve();
+                        }
+                    }
+                }
+            }
+
+            if self.active_hex == addr {
+                // Draw the active hex with a grey border.
+                ctx.set_source_rgb(0.3, 0.3, 0.3);
+                ctx.set_line_width(hex.max_d * 0.02);
+                hex.define_boundary(ctx);
+                ctx.stroke();
+
+                // Highlight the active token space.
+                // NOTE: this still needs to be done, as the active token
+                // space may be empty and thus no spaces will be highlighted
+                // by the code above.
+                if let Some((tile, _tokens)) = tile_opt {
+                    let token_space = &self.token_spaces[self.selected];
+                    tile.define_token_space(token_space, hex, ctx);
+                    ctx.set_source_rgb(0.8, 0.2, 0.2);
+                    ctx.set_line_width(hex.max_d * 0.025);
+                    ctx.stroke_preserve();
+                }
+            } else {
+                // Cover all other tiles with a partially-transparent layer.
+                ctx.set_source_rgba(1.0, 1.0, 1.0, 0.25);
+                hex.define_boundary(ctx);
+                ctx.fill();
+            }
+        }
+    }
+
+    fn key_press(
+        mut self: Box<Self>,
+        _hex: &Hex,
+        map: &mut Map,
+        _window: &gtk::ApplicationWindow,
+        _area: &gtk::DrawingArea,
+        event: &gdk::EventKey,
+    ) -> (Box<dyn State>, Inhibit, Action) {
+        let key = event.get_keyval();
+        match key {
+            gdk::enums::key::q | gdk::enums::key::Q => {
+                (self, Inhibit(false), Action::Quit)
+            }
+            gdk::enums::key::Escape | gdk::enums::key::Return => {
+                // Exit this mode.
+                let state = Box::new(Default {
+                    active_hex: Some(self.active_hex),
+                });
+                (state, Inhibit(false), Action::Redraw)
+            }
+            gdk::enums::key::Left => {
+                if self.selected == 0 {
+                    self.selected = self.token_spaces.len() - 1;
+                } else {
+                    self.selected -= 1
+                }
+                if let Some(hex_state) = map.get_hex(self.active_hex) {
+                    // Update the matching tokens across the map.
+                    let space = self.token_spaces[self.selected];
+                    let token_opt = hex_state.get_token_at(&space);
+                    self.matches = token_matches(map, token_opt);
+                    (self, Inhibit(false), Action::Redraw)
+                } else {
+                    (self, Inhibit(false), Action::None)
+                }
+            }
+            gdk::enums::key::Right => {
+                self.selected += 1;
+                if self.selected >= self.token_spaces.len() {
+                    self.selected = 0
+                }
+                if let Some(hex_state) = map.get_hex(self.active_hex) {
+                    // Update the matching tokens across the map.
+                    let space = self.token_spaces[self.selected];
+                    let token_opt = hex_state.get_token_at(&space);
+                    self.matches = token_matches(map, token_opt);
+                    (self, Inhibit(false), Action::Redraw)
+                } else {
+                    (self, Inhibit(false), Action::None)
+                }
             }
             _ => (self, Inhibit(false), Action::None),
         }
