abf623d24d145fd9cc362019b26df3c6f1fdda09
Rob Moss <robm.dev@gmail.com>
2020-03-14 16:51:13 +1100

    Only consider pairing a train to each path
    This avoids considering pairing trains to only a subset of the paths in
    the current path combination.

--- src/route/train.rs
+++ src/route/train.rs
@@ -424,7 +424,9 @@ impl Trains {
     ) -> Option<(usize, Vec<(Train, usize, usize)>)> {
         let num_paths = path_ixs.len();
         let num_trains = self.train_count();
-        let train_combinations = Combinations::new(num_trains, num_paths);
+        // NOTE: we only need to consider pairings that allocate a train to
+        // each path, we can can ignore smaller combinations.
+        let train_combinations = KCombinations::new(num_trains, num_paths);
 
         train_combinations
             .filter_map(|train_ixs| {
@@ -585,9 +587,61 @@ impl<F: Fn(usize, usize) -> bool> Iterator for CombinationsFilter<F> {
     }
 }
 
+/// Iterate over *k*-combinations of a set of size *n*.
+pub struct KCombinations {
+    item_count: usize,
+    want_len: usize,
+    items: Vec<usize>,
+    current_ix: usize,
+}
+
+impl KCombinations {
+    /// Create an iterator over *k*-combinations of a set of size *n*.
+    pub fn new(n: usize, k: usize) -> Self {
+        KCombinations {
+            item_count: n,
+            want_len: k,
+            items: Vec::with_capacity(k),
+            current_ix: 0,
+        }
+    }
+}
+
+impl Iterator for KCombinations {
+    type Item = Vec<usize>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_ix >= self.item_count {
+            // Reached the end of nesting, pop.
+            if let Some(prev_ix) = self.items.pop() {
+                // Move to the next sibling, and rely on recursive searching.
+                self.current_ix = prev_ix + 1;
+                self.next()
+            } else {
+                // Have iterated over all possible combinations.
+                None
+            }
+        } else {
+            self.items.push(self.current_ix);
+            if self.items.len() < self.want_len {
+                // Prepare to descend, starting at the smallest value that
+                // hasn't already been included in the current combination.
+                self.current_ix = self.items.iter().max().unwrap() + 1;
+                self.next()
+            } else {
+                let item = Some(self.items.clone());
+                // Prepare to move to the next sibling.
+                self.items.pop();
+                self.current_ix += 1;
+                item
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{Combinations, CombinationsFilter};
+    use super::{Combinations, CombinationsFilter, KCombinations};
     use log::info;
 
     fn init() {
@@ -634,6 +688,33 @@ mod tests {
         assert_eq!(expected_count, combs.len());
         for c in &combs {
             info!("{:?}", c);
+        }
+    }
+
+    #[test]
+    /// Check that for a set of size 5 there are:
+    ///
+    /// - 5 x *1*-combinations (0..4);
+    /// - 10 x *2*-combinations: 5! / (2! * 3!) = 20 / 2 = 10; and
+    /// - 10 x *3*-combinations: 5! / (3! * 2!) = 10.
+    fn test_kcombinations_1() {
+        init();
+        let ks: [usize; 3] = [1, 2, 3];
+        for k in &ks {
+            let comb = KCombinations::new(5, *k);
+            let combs: Vec<_> = comb.collect();
+            if *k == 1 {
+                assert_eq!(5, combs.len())
+            } else if *k == 2 {
+                assert_eq!(10, combs.len())
+            } else if *k == 3 {
+                assert_eq!(10, combs.len())
+            } else {
+                unreachable!("k should be in [1, 2, 2]")
+            }
+            for comb in &combs {
+                assert_eq!(comb.len(), *k)
+            }
         }
     }
 }
