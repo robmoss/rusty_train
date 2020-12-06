d59825022f2a01415d7a57845029f284dffb78b0
Rob Moss <robm.dev@gmail.com>
2020-12-06 13:59:08 +1100

    Update the current features status

--- crates/navig18xx/src/lib.rs
+++ crates/navig18xx/src/lib.rs
@@ -12,12 +12,12 @@
 //! - (De)serialising tile descriptions.
 //! - Placing tokens in token spaces.
 //! - Searching maps for optimal pairings of trains to routes.
-//!
-//! The following features are not yet provided:
-//!
-//! - Defining and manipulating 18xx game maps.
 //! - Allowing for route bonuses.
 //! - Selecting trains to operate routes for a company.
+//!
+//! The following features are **partially** implemented:
+//!
+//! - Defining and manipulating 18xx game maps.
 //!
 //! Note: there **is** support for finding a single route that yields the
 //! highest possible revenue (ignoring bonuses for, e.g., visiting specific
@@ -128,29 +128,3 @@ pub use n18route as route;
 pub use n18tile as tile;
 pub use n18token as token;
 pub use n18ui as ui;
-
-// TODO:
-//   grid module for tile arrangement
-//   map module for building on top of grid and having off-grid content?
-//   ui module for commands and actions (e.g., manipulating map)
-//   route module for finding optimal routes and revenue
-//   token module for helping define possible routes
-//   (may need some modifications to City)
-//   a (de)serialisation module for Tile/Map definitions?
-//
-//   more general handling of labels and revenue circles, able to position
-//   them relative to centre / face / corner, with optional nudge ...
-//
-//   TEST CASES!
-//   https://doc.rust-lang.org/1.30.0/book/second-edition/ch11-03-test-organization.html
-//   -> no track should escape its hex
-//   -> all coords should lie on the track
-//   -> check start/end
-//   -> check that coords approach the start/end
-//   -> check coords are clipped
-//   -> check x0/x1 limits are correct
-//      -> e.g., that limited tracks connect, but don't cross
-//   -> draw every possible segment from every possible face
-//      -> iterate over specific x0/x1 and c0/c1 combinations
-//   -> ensure topology of track/track and track/city connections are correct
-//      for many (most? all?) tiles in catalogue
