robmoss/rusty_train

commit 0be157c7f238c725ee2fe7f2da52de01222391a0

Support selecting games by name or index

crates/n18ui/src/state/find_routes.rs
@@ -32,10 +32,9 @@ impl FindRoutes {
         let companies = valid_companies(content);
         let company_names: Vec<&str> =
             companies.iter().map(|c| c.full_name.as_str()).collect();
-        let ix = select_item(window, "Select a company", &company_names)?;
+        let abbrev = select_item(window, "Select a company", &company_names)?;
 
-        // Identify the company name and token.
-        let abbrev = &companies[ix].abbrev;
+        // Identify the company token.
         let token = content.map.try_token(abbrev)?;
 
         // Select the company trains and bonuses.
@@ -52,7 +51,7 @@ impl FindRoutes {
             window.get_title().map(|s| s.as_str().to_string());
         let state = FindRoutes {
             active_hex: active_hex.copied(),
-            abbrev: abbrev.clone(),
+            abbrev: abbrev.to_string(),
             best_routes,
             original_window_title,
             active_route,

crates/n18ui/src/dialog.rs
@@ -75,7 +75,7 @@ impl SelectItemDialog {
 }
 
 /// Display an item-selection dialog and return the selected item index.
-pub fn select_item<'a>(
+pub fn select_item_index<'a>(
     parent: &gtk::ApplicationWindow,
     title: &str,
     items: &'a [&'a str],
@@ -85,6 +85,15 @@ pub fn select_item<'a>(
     }
     let dlg = SelectItemDialog::new(parent, title, items);
     dlg.run()
+}
+
+/// Display an item-selection dialog and return the selected item.
+pub fn select_item<'a>(
+    parent: &gtk::ApplicationWindow,
+    title: &str,
+    items: &'a [&'a str],
+) -> Option<&'a str> {
+    select_item_index(parent, title, items).map(|ix| items[ix])
 }
 
 /// A dialog for selecting trains and options that provide route bonuses.

crates/n18ui/src/lib.rs
@@ -54,8 +54,24 @@ impl Games {
         self.games.iter().map(|g| &**g)
     }
 
-    /// Changes the active game.
-    pub fn set_active(&mut self, ix: usize) -> bool {
+    /// Changes the active game by name.
+    pub fn set_active_name(&mut self, name: &str) -> bool {
+        let ix_opt = self
+            .games
+            .iter()
+            .enumerate()
+            .find(|(_ix, game)| game.name() == name)
+            .map(|(ix, _game)| ix);
+        if let Some(ix) = ix_opt {
+            self.game_ix = ix;
+            true
+        } else {
+            false
+        }
+    }
+
+    /// Changes the active game by index.
+    pub fn set_active_index(&mut self, ix: usize) -> bool {
         if ix < self.games.len() {
             self.game_ix = ix;
             true
@@ -446,10 +462,13 @@ pub fn global_keymap(
         (gdk::keys::constants::n, true) | (gdk::keys::constants::N, true) => {
             // Prompt the user to select a game, and load its starting map.
             let game_names: Vec<&str> = content.games.names();
-            let ix_opt =
-                dialog::select_item(window, "Select a game", &game_names);
+            let ix_opt = dialog::select_item_index(
+                window,
+                "Select a game",
+                &game_names,
+            );
             if let Some(ix) = ix_opt {
-                if content.games.set_active(ix) {
+                if content.games.set_active_index(ix) {
                     content.map =
                         content.games.active().create_map(&content.hex);
                     window.set_title(content.games.active().name());

crates/n18ui/src/util.rs
@@ -159,16 +159,7 @@ pub fn load_game(
         return Ok(Action::None);
     };
     let game_state = n18io::read_game_state(dest_file)?;
-    let game_opt: Option<usize> =
-        content.games.games.iter().enumerate().find_map(|(ix, g)| {
-            if g.name() == game_state.game {
-                Some(ix)
-            } else {
-                None
-            }
-        });
-    if let Some(ix) = game_opt {
-        content.games.set_active(ix);
+    if content.games.set_active_name(&game_state.game) {
         if let Some(new_map) =
             content.games.active_mut().load(&content.hex, game_state)
         {

commit 4bd4d819543a1721425fa1d333b00d9bf896b432

Fix searching for tokens by company abbreviation
This corrects a bug introduced in commit 0be157c, where the full company
name was used instead of the abbreviation to search for matching tokens.

crates/n18ui/src/state/find_routes.rs
@@ -32,7 +32,15 @@ impl FindRoutes {
         let companies = valid_companies(content);
         let company_names: Vec<&str> =
             companies.iter().map(|c| c.full_name.as_str()).collect();
-        let abbrev = select_item(window, "Select a company", &company_names)?;
+        let chosen_name =
+            select_item(window, "Select a company", &company_names)?;
+        let abbrev = companies.iter().find_map(|c| {
+            if c.full_name == chosen_name {
+                Some(c.abbrev.as_str())
+            } else {
+                None
+            }
+        })?;
 
         // Identify the company token.
         let token = content.map.try_token(abbrev)?;